f98eee84186cd024ca3bd5ffbb71291c967277bb
Your Name <you@example.com>
2023-05-02 21:20:51 +0300

    MyBlogPage
    I talked about myself on this website that I created using HTML, CSS, and JS. My CV information is also included in it

---
+++ meltemSavas201816061 WebVizesi/main.js
@@ -0,0 +1,10 @@
+hamburger = document.querySelector(".hamburger");
+nav = document.querySelector(".nav-bar");
+hamburger.onclick = function () {
+  nav.classList.toggle("active");
+};
+
+$(document).ready(function() {
+  $("#kimimben").addClass("animate");
+  });
+
